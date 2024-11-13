0e87733f70c3c34ae665b9ffe65ff7ba72e871dc
Marshall Hotaling <marshall.hotaling@gmail.com>
2024-11-13 01:20:05 -0700

    css styling
    styled with theam and styling

--- src/component/articleView/articleView.jsx
+++ src/component/articleView/articleView.jsx
@@ -18,13 +18,13 @@ const ArticleView = () => {
           <label className="title" onClick={() => navigate(`/`)}> Back</label>
         </nav>
         <div className="singleScroolView">
-          <label className="articleLabel1">Date : {state.publishedAt}</label>
-          <label className="articleLabel2">Source : {state.source.name}</label>
-          <label className="articleLabel3">Author : {state.author}</label>
-          <img className="singleNewsImg" src={state.urlToImage} alt={""}/>
-          <label className="articleLabel4">{state.title}</label>
-          <label className="articleLabel5">{state.description}</label>
-          <label className="articleLabel6">{state.content}</label>
+          <img className="articleImage" src={state.urlToImage} alt={""}/>
+          <label className="articleSource">Source : {state.source.name}</label>
+          <label className="articleAuthor">Author : {state.author}</label>
+          <label className="articleDate">Date : {state.publishedAt}</label>
+          <label className="articleTitle">{state.title}</label>
+          <label className="articleDescription">{state.description}</label>
+          <label className="articleContent">{state.content}</label>
           <label className="articleLabel7">For More Read Original Article at : <a
             href={state.url}>{state.url}</a></label>
         </div>
